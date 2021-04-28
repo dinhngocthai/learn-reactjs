881bbf430f276bf53ed24fc47ce430a0cbf7da73
DinhNgocThai <73975309+dinhngocthai@users.noreply.github.com>
2021-04-28 23:09:22 +0700

    test

---
+++ src/feature/Todo/TodoList/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from "classnames";
+TodoList.propTypes = {
+    todoList: PropTypes.array,
+};
+TodoList.defaultProps = {
+    todoList: [],
+};
+
+function TodoList({ todoList }) {
+    return (
+        <ul>
+            {todoList.map(todo => (
+                <li key={todo.id} classNames= {classNames({completed: todo.status === 'completed'})} >
+                    {todo.title}
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default TodoList;
