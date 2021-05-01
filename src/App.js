62a6b0c79eacb1b5370ca7e86780ae854f495695
DinhNgocThai <73975309+dinhngocthai@users.noreply.github.com>
2021-05-02 01:04:40 +0700

    Studying Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.css';
-import ColorBox from './components/colorBox/index';
+import React from 'react';
+import './App.scss';
+import {Link, Route, Switch } from 'react-router-dom';
+
+import TodoFeature from './feature/Todo/pages';
+import ColorBox from './feature/ColorBox';
+import BetterClock from './feature/BetterClock';
+
 
 function App() {
+
   return (
-      <ColorBox/>
+
+    <div className="app">
+      <div>Home Page</div>
+      <p> <Link to="/todos">Go to Todo    </Link>
+      <Link to="/colorBox">Go to ColorBox    </Link>
+      <Link to="/betterClock">Go to BetterClock    </Link>
+      </p>
+      <p> </p>
+      {/* nav link khac link o cho them class active vao link dc chon hien tai */}
+      {/* de loc nhung components trung nhau tai mot thoi diem*/}
+      <Switch>
+      <Route path="/todos" component={TodoFeature} />
+      <Route path="/todos" component={TodoFeature} />
+      <Route path="/todos" component={TodoFeature} />
+
+      <Route path="/colorBox" component={ColorBox} />
+      <Route path="/betterClock" component={BetterClock} />
+      </Switch>
+    </div>
   );
 }
 
 export default App;
-
--- src/feature/Todo/Pages/index.jsx
+++ src/feature/Todo/Pages/index.jsx
@@ -1,35 +1,70 @@
-import React from 'react';
-import TodoList from '../TodoList';
+import React, { useState } from 'react';
+import TodoList from '../components/TodoList';
 
 TodoFeature.propTypes = {
 
 };
 
 function TodoFeature(props) {
-    const todoList = [
+    const initTodoList = [
         {
             id: 1,
-            title: 'Eat',
+            title: 'eat',
             status: 'new',
-
         },
         {
             id: 2,
-            title: 'Sleep',
+            title: 'sleep',
             status: 'completed',
-
         },
         {
             id: 3,
-            title: 'Code',
+            title: 'code',
             status: 'new',
-
         },
-    ]
+    ];
+    //su dung state cho bien initTodoList
+    const [todoList, setTodoList] = useState(initTodoList);
+    const [filteredStatus, setfilterStatus] = useState('all');
+    //tao 1 ham thong bao khi nhan vao
+    const handleTodoClick = (todo, idx) => {
+        //muon thay doi phai clone no ra mang moi
+        // clone current array to the new one
+        const newTodoList = [...todoList];
+        console.log(todo, idx);
+        // toogle state
+        //gop ham newTodoList[idx]= newTodo;
+
+        newTodoList[idx] = {
+            ...newTodoList[idx],
+            status: newTodoList[idx].status === 'new' ? 'completed' : 'new',
+        };
+        //update todo list
+        setTodoList(newTodoList);
+    }
+    const handleShowAllClick = () => {
+        setfilterStatus('all');
+    };
+    const handleShowCompletedClick = () => {
+        setfilterStatus('completed');
+    };
+    const handleShowNewClick = () => {
+        setfilterStatus('new');
+    };
+    const renderedTodoList = todoList.filter(todo => filteredStatus === 'all' || filteredStatus === todo.status)
+    console.log(renderedTodoList);
     return (
         <div>
-            <h3>Todo List </h3>
-            <TodoList todoList= {todoList}/>
+            <h3>Todo List</h3>
+            <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
+            <div>
+                <button onClick={handleShowAllClick}>Show All</button>
+                <button onClick={handleShowCompletedClick}>Show Completed</button>
+                <button onClick={handleShowNewClick}>Show New</button>
+
+            </div>
+
+
         </div>
     );
 }
